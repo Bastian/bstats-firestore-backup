e942a0af9835cb5f5458824899da53444a7b57b1
Bastian <bastianoppermann1997@gmail.com>
2022-02-04 16:02:31 +0100

    Update database path

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,7 +8,7 @@ export const runBackup = functions.pubsub
     const client = new firestore.v1.FirestoreAdminClient();
 
     const projectId = process.env.GCP_PROJECT || process.env.GCLOUD_PROJECT;
-    const databaseName = client.databasePath(projectId, 'default');
+    const databaseName = client.databasePath(projectId, '(default)');
 
     await client.exportDocuments({
       name: databaseName,
